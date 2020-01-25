b331d652cc4dfdcaf80598c61d55d567ad5d4d10
TamaraShy <alisashylko@gmail.com>
2020-01-25 20:54:52 +0200

    add Configuration

---
+++ src/test/java/Configuration/Browser.java
@@ -0,0 +1,30 @@
+package Configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Driver;
+
+public enum Browser {
+    CHROME ( new ChromeDriver()),
+    FIREFOX (new FirefoxDriver());
+
+
+    public WebDriver driver;
+
+    Browser(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
+
